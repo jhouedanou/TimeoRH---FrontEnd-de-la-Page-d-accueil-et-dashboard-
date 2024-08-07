aa23960ef6d6f87bc21f28db87996133f11a8a03
Jean Luc Houédanou <jhouedanou@gmail.com>
2024-08-07 08:12:58 +0000

    `ùlmk;ù`

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,11 @@
 <script setup>
-import { useIndexContent } from "/composables/index";
+import { useindex } from "/composables/index";
 import FrontendLayout from "/layouts/frontend.vue";
 const { content } = useindex();
 </script>
 
 <template>
   <FrontendLayout>
-    <div class="container">
-      <h1>{{ content.siteName }}</h1>
-      <img :src="content.logo" :alt="content.siteName" />
-      <p>{{ content.slogan }}</p>
-      <router-link to="/connexion">Connexion</router-link>
-    </div>
+    <div class="container"></div>
   </FrontendLayout>
 </template>
