a69d462068bc343ae19dd9aa8eeeacd31a1c1a12
Jean Luc Houédanou <jhouedanou@gmail.com>
2024-09-12 18:34:24 +0000

    filters oh

--- components/GraphiqueHomePage.vue
+++ components/GraphiqueHomePage.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="chart-container">
-    <button @click="toggleDataSet">
-      {{
-        isYearlyData
-          ? "Afficher données mensuelles"
-          : "Afficher données annuelles"
-      }}
-    </button>
+    <div class="filters">
+      <button @click="toggleFilter('pourvues')" :class="{ active: activeFilters.includes('pourvues') }">
+        Offres pourvues
+      </button>
+      <button @click="toggleFilter('nonPourvues')" :class="{ active: activeFilters.includes('nonPourvues') }">
+        Offres non pourvues
+      </button>
+      <button @click="toggleDataSet">
+        {{
+          isYearlyData
+            ? "Afficher données mensuelles"
+            : "Afficher données annuelles"
+        }}
+      </button>
+    </div>
+
     <ClientOnly>
-      <apexchart
-        v-if="chartData"
-        width="100%"
-        height="350"
-        type="bar"
-        :options="chartOptions"
-        :series="currentSeries"
-      ></apexchart>
+      <apexchart v-if="chartData" width="100%" height="350" type="bar" :options="chartOptions" :series="currentSeries">
+      </apexchart>
     </ClientOnly>
   </div>
 </template>
@@ -23,43 +26,93 @@
 <script setup>
 import { ref, computed } from "vue";
 import { useChartData } from "~/composables/useChartData";
+const activeFilters = ref(['pourvues', 'nonPourvues']);
 
 const { chartData } = useChartData();
 
 const isYearlyData = ref(true);
 
-const currentSeries = computed(() =>
-  isYearlyData.value
-    ? chartData.value.yearlySeries
-    : chartData.value.monthlySeries
-);
+const currentSeries = computed(() => {
+  const series = isYearlyData.value ? chartData.value.yearlySeries : chartData.value.monthlySeries;
+  return series.filter(s =>
+    (s.name === "Offres pourvues" && activeFilters.value.includes('pourvues')) ||
+    (s.name === "Offres non pourvues" && activeFilters.value.includes('nonPourvues'))
+  );
+});
 
 const chartOptions = computed(() => ({
   chart: {
     id: "vuechart-example",
-    stacked: true,
+    type: "bar",
   },
-  colors: ["#012e61", "#dc9756"],
+  colors: ["#4CAF50", "#FF5722"], // Vert pour pourvues, Rouge pour non pourvues
   plotOptions: {
     bar: {
-      columnWidth: "80%",
-      borderRadius: 10,
+      horizontal: false,
+      columnWidth: "40%",
+      endingShape: "rounded",
+      distributed: false,
+      dataLabels: {
+        position: "top",
+      },
     },
+  },
+  dataLabels: {
+    enabled: true,
+    formatter: function (val) {
+      return val;
+    },
+    offsetY: -20,
+    style: {
+      fontSize: "12px",
+      colors: ["#304758"]
+    }
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ["transparent"]
   },
   xaxis: {
     categories: isYearlyData.value
       ? chartData.value.monthsFr
       : Array.from({ length: 30 }, (_, i) => i + 1),
   },
+  yaxis: {
+    title: {
+      text: "Nombre d'offres"
+    }
+  },
+  fill: {
+    opacity: 1
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return val + " offres"
+      }
+    }
+  },
   title: {
-    text: isYearlyData.value ? "Données annuelles" : "Données mensuelles",
+    text: isYearlyData.value ? "Offres pourvues vs non pourvues (Annuel)" : "Offres pourvues vs non pourvues (Mensuel)",
     align: "center",
   },
+  legend: {
+    position: "bottom"
+  }
 }));
-
 function toggleDataSet() {
   isYearlyData.value = !isYearlyData.value;
 }
+function toggleFilter(filter) {
+  const index = activeFilters.value.indexOf(filter);
+  if (index > -1) {
+    activeFilters.value.splice(index, 1);
+  } else {
+    activeFilters.value.push(filter);
+  }
+}
+
 </script>
 <style lang="scss">
 .chart-container {
