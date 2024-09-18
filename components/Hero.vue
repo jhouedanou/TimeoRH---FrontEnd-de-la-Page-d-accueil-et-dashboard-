f4f6856ec18e32ad9473ce0abac44a43869f2d13
Jean Luc Houedanou <jeanluc@houedanou.com>
2024-09-18 15:37:45 +0000

    Modifications de la page d'accueil

--- components/Hero.vue
+++ components/Hero.vue
@@ -22,7 +22,7 @@ const icons = ["people_outline", "handshake", "trending_up"];
   <div class="herowrapper" :style="backgroundStyle">
     <div id="hero" class="container">
       <h1 class="inter mt-6 mb-6" v-html="data.titre"></h1>
-      <h4 class="inter mb-6" v-html="data.soustitre"></h4>
+      <!-- <h4 class="inter mb-6" v-html="data.soustitre"></h4> -->
 
       <div class="search-container Rectangle-2">
         <input type="text" :placeholder="data.searchbox" class="input is-medium" v-model="searchQuery"
@@ -31,13 +31,6 @@ const icons = ["people_outline", "handshake", "trending_up"];
           <img src="/images/searchBtn.svg" alt="Search bouton" />
         </NuxtLink>
       </div>
-      <ul class="columns sous-sections">
-        <li class="column is-4-desktop is-4-mobile" v-for="(section, index) in data.sousSections" :key="index">
-          <span class="material-icons">{{ icons[index] }}</span>
-          <p>{{ section }}</p>
-
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -67,70 +60,6 @@ const icons = ["people_outline", "handshake", "trending_up"];
     bottom: -145px;
     z-index: 10000;
     justify-content: space-evenly;
-  }
-}
-
-.sous-sections {
-  list-style-type: none;
-  padding: 0;
-  margin-bottom: 20px;
-  position: absolute;
-  left: 0;
-  right: 0;
-  margin: 0 auto -5em auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-
-  li {
-    margin-bottom: 10px;
-    font-size: 18px;
-    color: #ffffff;
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    flex-direction: column;
-
-    p {
-      font-family: "Inter", sans-serif;
-      font-size: 18px;
-      font-weight: 800;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: 1.56;
-      letter-spacing: normal;
-      text-align: center;
-      color: #000;
-      margin: 1em 0;
-      text-align: center;
-      max-width: 200px;
-      white-space: pre-wrap;
-    }
-
-    .material-icons {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 140px;
-      height: 140px;
-      padding: 10px;
-      border-radius: 50%;
-      background-color: #ffffff;
-      color: #022C5E;
-      margin-right: 10px;
-      font-size: 4em;
-      border: 4px solid transparent;
-      box-shadow: 0 0 0 14px rgba(255, 255, 255, 0.5), 0 0 20px rgba(255, 255, 255, 0.3);
-      transition: all 0.3s ease;
-
-      &:hover {
-        background-color: #022C5E;
-        color: #ffffff;
-      }
-
-
-    }
   }
 }
 </style>
--- components/homepageRecruteurs.vue
+++ components/homepageRecruteurs.vue
@@ -6,12 +6,10 @@
     </div>
     <div class="container">
       <div class="columns is-desktop">
-        <div
-          v-for="(box, index) in data.recrutementBlock"
-          :key="index"
-          class="column is-4-desktop is-12-mobile is-flex is-align-items-center is-flex-direction-column is-justify-content-center recruitcolumn"
-        >
-          <img :src="box.image" :alt="'Image ' + (index + 1)" />
+        <div v-for="(box, index) in data.recrutementBlock" :key="index"
+          class="column is-4-desktop is-12-mobile is-flex is-align-items-center is-flex-direction-column is-justify-content-center recruitcolumn">
+          <span class="material-icons">{{ icons[index] }}</span>
+
           <h2>{{ box.titre }}</h2>
           <p>{{ box.description }}</p>
         </div>
@@ -22,5 +20,19 @@
 <script setup>
 import { useGlobalData } from "@/composables/useGlobalData";
 const { data } = useGlobalData();
+const icons = ["people_outline", "handshake", "trending_up"];
+
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.material-icons {
+  background: #dc9756;
+  font-size: 4em;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: #f3b173;
+  border-radius: 50%;
+}
+</style>
